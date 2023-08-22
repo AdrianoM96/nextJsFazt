d4ea1913cb8230b167cedca9d1c842d9613108c0
AdrianoM96 <adrym09@gmail.com>
2023-08-22 18:21:44 -0300

    Link

---
+++ app/tienda/layout.jsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+export default function TiendaLayout({children}) {
+    return <>
+    
+        <nav>
+            <h3>Seccion Tienda   </h3>
+            <ul>
+                <li>
+                   <Link href="/tienda/categorias">Categorias</Link> 
+                </li>
+            </ul>
+            <li>
+                <Link href="/tienda/categorias/computadoras">Computadoras</Link>
+            </li>
+
+        </nav>
+    
+    {children}</>
+}
